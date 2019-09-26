9e5c71239a4dfd45f67167aec51db039ba574c00
Alex <bubencov0110@mail.ru>
2019-09-27 00:23:59 +0700

    v1.1(oop_version)

--- main.cpp
+++ main.cpp
@@ -2,67 +2,69 @@
 #include <math.h>
 #include <locale.h>
 
-typedef struct{
-           float lenBase, angle;
-}Triangle;
+#include <iostream>
 
-void Init(Triangle a)
-{
-    a.lenBase = 0;
-    a.angle = 0;
-}
+using namespace std;
 
-void Read(Triangle* a)
-{
-    printf("Введите длину основания:");
-    scanf("%f", &a->lenBase);
-    printf("Введите угол напротив основания:");
-    scanf("%f", &a->angle);
-}
-
-void Display(Triangle a)
-{
-    printf("Длина основания: %g\n", a.lenBase);
-    printf("Угол напротив основания: %g\n", a.angle);
-}
-
-Triangle Add(Triangle a, Triangle b)
-{
-    a.lenBase = a.lenBase + b.lenBase;
-    a.angle = (a.angle + b.angle) / 2;
-    return a;
-}
-
-float S(Triangle a)
-{
-    float h = a.lenBase / 2 / tan(a.angle / 2);
-    return a.lenBase * h / 2;
-}
+class Triangle{
+    private:
+        float lenBase;
+        float angle;
+    public:
+        Triangle(float Base, float Angle)
+        {
+            lenBase = Base;
+            angle = Angle;
+        }
+        ~Triangle(){}
+        void Read()
+        {
+            cout << "Введите длину основания:";
+            cin >> this->lenBase;
+            cout << "Введите угол напротив основания:";
+            cin >> this->angle;
+        }
+        void Display()
+        {
+            cout << "Длина основания:" << this->lenBase << endl;
+            cout << "Угол напротив основания:" << this->angle << endl;
+        }
+        void Add(Triangle a)
+        {
+            this->lenBase = this->lenBase + a.lenBase;
+            this->angle = (this->angle + a.angle) / 2;
+        }
+        float S()
+        {
+            float h = this->lenBase / 2 / tan(this->angle / 2);
+            return (this->lenBase * h / 2);
+        }
+};
 
 int main()
 {
     setlocale(0, "");
-    Triangle triangle1, triangle2;
-    Init(triangle1);
-    Init(triangle2);
+    Triangle triangle1(0, 0), triangle2(0, 0);
     int exit = 0;
     while(exit == 0)
     {
         printf("\tЗадан треугольник (длина основания, угол напротив основания), Имеются функции для работы с данными треугольника(ов).\n");
         printf("1)Read(Ввод данных треугольника с клавиатуры)\n");
         printf("2)Display(Вывод данных треугольника на экран)\n");
-        printf("3)Add(сложение двух треугольников)\n");
+        printf("3)Add(сложение двух треугольников. результат в переменные первого треугольника)\n");
         printf("4)S(площадь треугольника)\n");
         printf("5)Выход\n");
         int var;
         scanf("%d", &var);
         switch(var)
         {
-            case 1: Read(&triangle1);                       break;
-            case 2: Display(triangle1);                     break;
-            case 3: Read(&triangle2);
-                    triangle1 = Add(triangle1, triangle2);  break;
-            case 4: printf("S = %f\n", S(triangle1));       break;
+            case 1: triangle1.Read();               break;
+            case 2: triangle1.Display();            break;
+            case 3: cout << "\tВведите данные второго треугольника\n";
+                    triangle2.Read();
+                    triangle1.Add(triangle2);       break;
+            case 4: cout << "S = " << triangle1.S() << endl;
+                                                    break;
             default:exit = 1;
         }
     }
